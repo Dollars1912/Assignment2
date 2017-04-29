45b1209fd25876967d0eae4f040c19e0f3c7659c
Lawrence Chu <DOLLARS1912@chuguanhongdeMacBook-Pro.local>
2017-04-29 13:50:05 +1000

    basic release table view

---
+++ Sneaker+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Sneaker+CoreDataClass.swift
+//  Assignment2
+//
+//  Created by 褚冠宏 on 29/04/2017.
+//  Copyright © 2017 lawrencechu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Sneaker: NSManagedObject {
+
+}
---
+++ Sneaker+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Sneaker+CoreDataProperties.swift
+//  Assignment2
+//
+//  Created by 褚冠宏 on 29/04/2017.
+//  Copyright © 2017 lawrencechu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Sneaker {
+
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Sneaker> {
+        return NSFetchRequest<Sneaker>(entityName: "Sneaker")
+    }
+
+    @NSManaged public var id: Int32
+    @NSManaged public var release_date: NSDate?
+    @NSManaged public var price: Float
+    @NSManaged public var name: String
+
+}
