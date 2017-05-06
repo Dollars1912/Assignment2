f18f78d2b4b04ef614ddebac8ec9a676cbd46da2
Lawrence Chu <DOLLARS1912@chuguanongdembp.home>
2017-05-06 12:18:27 +1000

    add sneaker detail view

--- Assignment2/ReleaseTableViewController.swift
+++ Assignment2/ReleaseTableViewController.swift
@@ -22,22 +22,8 @@ class ReleaseTableViewController: UITableViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let data = NSEntityDescription.insertNewObject(forEntityName: "Sneaker", into: managedObjectContext) as? Sneaker
-        data!.id = 1
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy/MM/dd HH:mm"
-        data!.release_date = formatter.date(from: "2016/10/08 22:31") as NSDate?
-        data!.price = 220.0
-        data!.name = "nike"
-        // save to db
-        
-        do {
-            try self.managedObjectContext.save()
-        } catch let error {
-            print("Could not save: \(error)")
-        }
-        
+
+        // read from db
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Sneaker")
         do {
             let result = try self.managedObjectContext.fetch(fetchRequest)
@@ -46,7 +32,41 @@ class ReleaseTableViewController: UITableViewController {
             let fetchError = error as NSError
             print(fetchError)
         }
-        print("--> \(sneakerList.count)")
+        loadFromDatabase()
+    }
+    
+    func loadFromDatabase() {
+        if (sneakerList.count > 0) {
+            return
+        }
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        var sneaker = Sneaker(context: managedObjectContext)
+        sneaker.id = 1
+        sneaker.release_date = formatter.date(from: "2016/10/08 22:31") as NSDate?
+        sneaker.price = 220.0
+        sneaker.name = "nike"
+        sneakerList.append(sneaker)
+        
+        sneaker = Sneaker(context: managedObjectContext)
+        sneaker.id = 2
+        sneaker.release_date = formatter.date(from: "2016/12/08 22:31") as NSDate?
+        sneaker.price = 120.0
+        sneaker.name = "puma"
+        sneakerList.append(sneaker)
+        
+        sneaker = Sneaker(context: managedObjectContext)
+        sneaker.id = 3
+        sneaker.release_date = formatter.date(from: "2016/11/08 22:31") as NSDate?
+        sneaker.price = 320.0
+        sneaker.name = "adidas"
+        sneakerList.append(sneaker)
+        // save to db
+        do {
+            try self.managedObjectContext.save()
+        } catch let error {
+            print("Could not save: \(error)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -69,53 +89,18 @@ class ReleaseTableViewController: UITableViewController {
         let sneaker: Sneaker = self.sneakerList[indexPath.row] as! Sneaker
         cell.textLabel?.text = "\(sneaker.name)"
         cell.detailTextLabel?.text = "\(sneaker.price)"
-        
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showSneakerDetail" {
+            let destination: SneakerDetailViewController = segue.destination as! SneakerDetailViewController
+            if let cell = sender as? UITableViewCell {
+                let indexPath = tableView.indexPath(for: cell)
+                let sneaker: Sneaker = sneakerList[indexPath!.row] as! Sneaker
+                destination.sneaker = sneaker
+            }
+        }
     }
-    */
 
 }
---
+++ Assignment2/SneakerDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  SneakerDetailViewController.swift
+//  Assignment2
+//
+//  Created by 褚冠宏 on 06/05/2017.
+//  Copyright © 2017 lawrencechu. All rights reserved.
+//
+
+import UIKit
+
+class SneakerDetailViewController: UIViewController {
+    var sneaker: Sneaker?
+
+    
+    @IBOutlet weak var sneakerNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if (sneaker != nil) {
+            sneakerNameLabel.text = sneaker!.name
+            priceLabel.text = "\(sneaker!.price)"
+        }
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
